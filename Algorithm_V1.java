5abe802f1584ac7f7252f67b374b1ce3ffd802ed
capitan america <Phuritry2222@gmail.com>
2026-05-15 14:15:09 +0700

    V0.0.03

--- Algorithm_V1.java
+++ Algorithm_V1.java
@@ -4,10 +4,10 @@ abstract class Algorithm_V1{
     int[] sample = {1 , 2, 3, 4, 5,};
 
     //method sum (Mr. AA)
-    abstract int sum();
+    abstract int sum(int[] input);
 
     //method average (Mr. BB)
-    abstract double average();
+    abstract double average(int[] input);
 
 
 }
