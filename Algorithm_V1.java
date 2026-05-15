e13818a49a4e0e8f444a9c2284a78140e6ad39d7
capitan america <Phuritry2222@gmail.com>
2026-05-15 14:54:32 +0700

    v0.0.04

--- Algorithm_V1.java
+++ Algorithm_V1.java
@@ -3,11 +3,17 @@ abstract class Algorithm_V1{
     //attribute 
     int[] sample = {1 , 2, 3, 4, 5,};
 
-    //method sum (Mr. AA)
-    abstract int sum(int[] input);
+    //method sum (Mr. lyna)
+    int sum(int[] input){
+        return new Algorithm_lyna().sum(input);
+    }
 
     //method average (Mr. BB)
-    abstract double average(int[] input);
+    // abstract double average(int[] input);
+    //  double average(int[] input){
+    //     return new Algorithm_lyna().average(input);
+    //  }
+   
 
 
 }
--- Algorithm_lyna.java
+++ Algorithm_lyna.java
@@ -6,24 +6,14 @@ public class Algorithm_lyna extends Algorithm_V1{
         int sum  = 0;
 
         for (int i = 0; i < 10; i++) {
-            sum = sum  + input[1];   
+            sum = sum  + input[i];   
         }
         return sum;
     }
-    
-    @Override
-    double average(int[] input) {
-        throw new UnsupportedOperationException("Unimplemented method 'average'");
-    }
-    
-       
     public static void main(String[] args) {
         Algorithm_V1 al = new Algorithm_lyna();
         System.out.println(Arrays.toString(al.sample));
         System.out.println(al.sum(al.sample));
-    }
-    
-
-    
+    }   
     
 }
