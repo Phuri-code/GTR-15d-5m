4a0034660ff74b4f13585a510df6a20e5a8207f5
sreasoknary466-hash <sreasoknary466@gmail.com>
2026-05-15 14:53:12 +0700

    hi all

--- Algorithm_nary.java
+++ Algorithm_nary.java
@@ -4,22 +4,28 @@ import java.util.Arrays;
 public class Algorithm_nary extends Algorithm_V1 {
 
     @Override
-    int sum() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sum'");
+    double average(int[] input) {
+        double sum = 0;
+        for (int i = 0; i < input.length; i++){
+            sum = sum + input[i];
+        } 
+        double avg = sum / input.length;
+        return avg;
     }
 
-    @Override
-    double average() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'average'");
-    }
 
+        
     public static void main(String[] args) {
         Algorithm_V1 al = new Algorithm_nary();
         System.out.println(Arrays.toString(al.sample));
         System.out.println(al.average(al.sample));
 
+    }
+
+    @Override
+    int sum(int[] input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sum'");
     }
 
     
