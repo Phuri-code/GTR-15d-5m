ab6b9e0c9576ae650870e7726f4041bdf9f183d1
capitan america <Phuritry2222@gmail.com>
2026-05-15 13:45:32 +0700

    first comit V0.0.01

---
+++ Alforithm.java
@@ -0,0 +1,13 @@
+abstract class Algorithm_V1{
+
+    //attribute 
+    int[] sample = {1 , 2, 3, 4, 5,};
+
+    //method sum (Mr. AA)
+    abstract int sum();
+
+    //method average (Mr. BB)
+    abstract double average();
+
+
+}
